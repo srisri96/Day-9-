532edf9fc98a924433047172fb72ddb1fa1e3b1d
srisri96 <srikanthsitharth80@gmil.com>
2022-07-19 19:39:17 +0530

    Contact address

---
+++ AddressBook/AddressBook/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Book
+    {
+        public class Address
+        {
+            List<Contacts> add_book = new List<Contacts>();
+            public void create()
+            {
+                Contacts contacts = new Contacts();
+                Console.WriteLine("Enter your first name");
+                contacts.first_name = Console.ReadLine();
+                Console.WriteLine("Enter your second name");
+                contacts.s_name = Console.ReadLine();
+                Console.WriteLine("Enter your address");
+                contacts.address = Console.ReadLine();
+                Console.WriteLine("Enter your city");
+                contacts.city = Console.ReadLine();
+                Console.WriteLine("Enter your state");
+                contacts.state = Console.ReadLine();
+            }
+        }
+        public void display()               //displaying details of each conatact
+        {
+            foreach (var data in add_book)
+            {
+                Console.WriteLine($"Contacts No. - {data}");
+                Console.WriteLine($"Name - {data.first_name} {data.s_name}");
+                Console.WriteLine($"Address - {data.address}");
+                Console.WriteLine($"City- {data.city}");
+                Console.WriteLine($"State - {data.state}");
+            }
+        }
+     }
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,6 @@
+﻿using AddressBook;
+Console.WriteLine("Welcome to Address Book Program in Address Book Main class on Master Branch");
+Console.WriteLine("Enter the details to create a contact");
+Address obj = new Address();
+obj.create();
+obj.display();
