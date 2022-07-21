21aab3a8613b609462f5968bf74e72a40485a86b
srisri96 <srikanthsitharth80@gmil.com>
2022-07-21 17:11:03 +0530

    Edited content

---
+++ Editcont/Editcont/Program.cs
@@ -0,0 +1,34 @@
+﻿using AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+Console.WriteLine("Welcome to Address Book Program in Address Book Main class on Master Branch");
+Console.WriteLine("Press 1 to add contact Press 2 to edit contact");
+Contact obj = new Contact();
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Enter the number of contacts needed to add");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Enter the details to create a contact");
+            obj.create();
+        }
+        Console.WriteLine("These are the contacts stored");
+        obj.display();
+        break;
+
+    case 2:
+        Console.WriteLine("Enter the first name of the contact you want to edit the details of");
+        string Firstname = Console.ReadLine();
+        Console.WriteLine("Enter the second name of the contact you want to edit the details of");
+        string s_name = Console.ReadLine();
+        obj.edit(Firstname, s_name);
+        obj.display();
+        break;
+}
